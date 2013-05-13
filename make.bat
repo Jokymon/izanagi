e381566cb02e053f7981c3fd92ee515a4eb03ae7
Silvan Wegmann <swegmann@narf.ch>
2013-05-13 21:20:04 +0200

    Cleanup commented out parts

--- make.bat
+++ make.bat
@@ -7,7 +7,6 @@ e:\vm-shared\i386-elf\bin\i386-elf-gcc.exe -c -o boot_hdisk.o boot_hdisk.S
 
 e:\vm-shared\i386-elf\bin\i386-elf-cpp.exe -P -I. barebox.lds.S -o barebox.lds -DTEXT_BASE=0x7c00
 e:\vm-shared\i386-elf\bin\i386-elf-ld.exe -nostdlib -T barebox.lds -e _start -o mbr.elf *.o
-REM boot_main.o boot_hdisk.o
 
 e:\vm-shared\i386-elf\bin\i386-elf-objcopy.exe -O binary mbr.elf mbr.bin
 e:\vm-shared\i386-elf\bin\i386-elf-objdump.exe -S -mi386 -Maddr16,data16,intel -d mbr.elf > mbr.lst
